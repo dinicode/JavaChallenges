83215c31ef774e94ce29af4d8c4107bd29d19fe2
Carldini Oreste <carldinioreste@gmail.com>
2023-04-17 15:59:55 -0400

    added several challenges

--- src/main/java/org/yup/Challenges.java
+++ src/main/java/org/yup/Challenges.java
@@ -1,7 +1,51 @@
 package org.yup;
 
+import java.util.Scanner;
+
 public class Challenges {
+    public String greeting(String name, int age) {
+        if (age >= 21) {
+            return "Hello " + name + ", come to the party!";
+        } else {
+            return "Sorry " + name + ", you are not old enough to attend the party.";
+        }
+    }
+
+    public String greetingWithUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String inputName = scanner.nextLine();
+        System.out.println("Enter your age:");
+        int inputAge = scanner.nextInt();
+        if (inputAge >= 21) {
+            return "Hello " + inputName + ", welcome to the party!";
+        }else{
+            return "Hello " + inputName + ", I am sorry but we cannot let you into the party.";
+        }
+    }
 
 
 
+
+    public int addTwoNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter the second number:");
+        int num2 = scanner.nextInt();
+        int sum = num1 + num2;
+        return sum;
+
+     }
+
+
+    public int subtractTwoNumbers() {
+        Scanner theScanner = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int num1 = theScanner.nextInt();
+        System.out.println("Enter the second number:");
+        int num2 = theScanner.nextInt();
+        int difference = num1 - num2;
+        return difference;
+    }
 }
--- src/main/java/org/yup/JavaChallengesApp.java
+++ src/main/java/org/yup/JavaChallengesApp.java
@@ -1,11 +1,27 @@
 package org.yup;
 
 public class JavaChallengesApp {
+Challenges challenges = new Challenges();
+
+
 
     public static void main (String[] args) {
 
-        System.out.println("Hello from java Challenges App");
 
+          //create a new instance of the challenges class so we can use the challenges in our application
+          Challenges challenges = new Challenges();
+
+          //this runs the greeting method from our challenges and returns a string greeting and
+          //stores it in the greeting varible we created
+          String greeting = challenges.greeting("Bobby",22);
+            System.out.println(greeting);
+
+            String greetingWithUserInput= challenges.greetingWithUserInput();
+
+            System.out.println(greetingWithUserInput);
+            int sum = challenges.subtractTwoNumbers();
+
+           System.out.println(sum);
     }
 
 }
