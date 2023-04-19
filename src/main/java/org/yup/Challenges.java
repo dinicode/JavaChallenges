08d38c94c07ad5d65f6f5aec27008e6a5707be04
Carldini Oreste <carldinioreste@gmail.com>
2023-04-19 13:20:03 -0400

    added new challenges

--- src/main/java/org/yup/Challenges.java
+++ src/main/java/org/yup/Challenges.java
@@ -19,12 +19,10 @@ public class Challenges {
         int inputAge = scanner.nextInt();
         if (inputAge >= 21) {
             return "Hello " + inputName + ", welcome to the party!";
-        }else{
+        } else {
             return "Hello " + inputName + ", I am sorry but we cannot let you into the party.";
         }
     }
-
-
 
 
     public int addTwoNumbers() {
@@ -32,11 +30,10 @@ public class Challenges {
         System.out.println("Enter the first number:");
         int num1 = scanner.nextInt();
         System.out.println("Enter the second number:");
-        int num2 = scanner.nextInt();
-        int sum = num1 + num2;
-        return sum;
+        int potato = scanner.nextInt();
 
-     }
+        return num1 + potato;
+    }
 
 
     public int subtractTwoNumbers() {
@@ -48,4 +45,154 @@ public class Challenges {
         int difference = num1 - num2;
         return difference;
     }
+
+
+    public int multiplyTwoNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter the second number:");
+        int potato = scanner.nextInt();
+        int potato2 = num1 * potato;
+        return potato2;
+    }
+
+
+    public int interactiveCalculator() {
+
+        Scanner theScanner = new Scanner(System.in);
+        System.out.println("Welcome to the calculator");
+        System.out.println("Press 1 to add numbers");
+        System.out.println("Press 2 to subtract numbers");
+        System.out.println("Press 3 to multiply numbers");
+        System.out.println("Enter your selection and press ENTER");
+        int userSelection = theScanner.nextInt();
+
+        int result = 0;
+
+        switch (userSelection) {
+            case 1:
+                result = this.addTwoNumbers();
+                // Call add method here and store the result
+                break;
+            case 2:
+                result = this.subtractTwoNumbers();
+                // Call subtract method here and store the result
+                break;
+            case 3:
+                result = this.multiplyTwoNumbers();
+                // Call multiply method here and store the result
+                break;
+            default:
+                System.out.println("The user chose an invalid option.");
+                return 0;
+        }
+
+        return result;
+    }
+
+
+    public String CoinFlip() {
+
+        //attempt to flip coin
+        //return heads or tails
+
+        if (Math.random() > .5) {
+            //here is where we return heads if the number generated was > 0.5
+            return "heads";
+        } else {
+            //otherwise, we return tails
+            return "tails";
+        }
+
+
+    }
+
+
+    public String findTheBiggestNumber() {
+
+        Scanner theScanner = new Scanner(System.in);
+        //this the scanner that lets us take input from user
+        System.out.println("give me the first number please: ");
+        //this where we capture that input and store it in a variable called firstNumber
+        int firstNumber = theScanner.nextInt();
+
+        //this the scanner that lets us take input from user
+        System.out.println("give me the first number please: ");
+        //this where we capture that input and store it in a variable called firstNumber
+        int secondNumber = theScanner.nextInt();
+
+        int theBiggestNumber = Math.max(firstNumber, secondNumber);
+
+        return "The biggest number between" + firstNumber + " and " + secondNumber + "is" + theBiggestNumber;
+
+    }
+
+
+    public int findtheDifferenceBetweenTwoNumbers() {
+        Scanner theScanner = new Scanner(System.in);
+
+        System.out.println("Give me the first number to subtract ");
+        int num1 = theScanner.nextInt();
+
+        System.out.println("Give me the second number to subtract ");
+        int num2 = theScanner.nextInt();
+
+        int difference = Math.abs(num1 - num2);
+
+        return difference;
+    }
+
+    public int findTheSmallestNumber() {
+        Scanner theScanner = new Scanner(System.in);
+
+        System.out.println("Give me the first number ");
+        int num1 = theScanner.nextInt();
+
+        System.out.println("Give me the second number ");
+        int num2 = theScanner.nextInt();
+
+        int smallestNumber = Math.min(num1, num2);
+
+        return smallestNumber;
+    }
+
+
+    public String convertToUppercase() {
+
+        //this is scanner that lets us take input from the user
+        Scanner theScanner = new Scanner(System.in);
+
+        System.out.println("Provide a word to convert all Uppercase");
+        String theWord = theScanner.nextLine();
+
+        return "You provided" + theWord + "and we converted to: " + theWord.toUpperCase();
+
+    }
+
+    public String changeToLowercase() {
+
+        //this is scanner that lets us take input from the user
+        Scanner theScan = new Scanner(System.in);
+
+        System.out.println("Provide a word to convert all lowercase:");
+        String Word = theScan.nextLine();
+
+        return "You provided " + Word + " and we converted to: " + Word.toLowerCase();
+    }
+
+    public String lengthOfTheWord() {
+
+        Scanner scan = new Scanner(System.in);
+        String Word = scan.nextLine();
+
+        return "The Word" + Word + " is " + Word.length() + "letters long";
+    }
+
 }
+
+
+
+
+
+
--- src/main/java/org/yup/JavaChallengesApp.java
+++ src/main/java/org/yup/JavaChallengesApp.java
@@ -1,31 +1,57 @@
 package org.yup;
 
 public class JavaChallengesApp {
-Challenges challenges = new Challenges();
+    Challenges challenges = new Challenges();
 
 
+    public static void main(String[] args) {
 
-    public static void main (String[] args) {
 
+        //create a new instance of the challenges class so we can use the challenges in our application
+        Challenges challenges = new Challenges();
+//
+//          //this runs the greeting method from our challenges and returns a string greeting and
+//          //stores it in the greeting varible we created
+//          String greeting = challenges.greeting("Bobby",22);
+//            System.out.println(greeting);
+//
+//            String greetingWithUserInput= challenges.greetingWithUserInput();
+//1
+//            System.out.println(greetingWithUserInput);
+//            int sumOfpotatoes = challenges.subtractTwoNumbers();
+//
+//           System.out.println(sumOfpotatoes);
+//       public String CoinFlip() {
+//
+//    //attempt to flip coin
+//    //return heads or tails
+//
+//    if (Math.random() > 0.5) {
+//        //here is where we return heads if the number generated was > 0.5
+//        return "heads";
+//    } else {
+//        //otherwise, we return tails
+//        return "tails";
+//    }
+//}
+//
 
-          //create a new instance of the challenges class so we can use the challenges in our application
-          Challenges challenges = new Challenges();
+//        System.out.println(challenges.CoinFlip());
 
-          //this runs the greeting method from our challenges and returns a string greeting and
-          //stores it in the greeting varible we created
-          String greeting = challenges.greeting("Bobby",22);
-            System.out.println(greeting);
+//        System.out.println(challenges.findTheBiggestNumber());
+//
+//        System.out.println(challenges.findTheSmallestNumber());
+//
+//        System.out.println(challenges.findtheDifferenceBetweenTwoNumbers());
 
-            String greetingWithUserInput= challenges.greetingWithUserInput();
+        System.out.println(challenges.convertToUppercase());
 
-            System.out.println(greetingWithUserInput);
-            int sum = challenges.subtractTwoNumbers();
+        System.out.println(challenges.changeToLowercase());
+        System.out.println(challenges.lengthOfTheWord());
 
-           System.out.println(sum);
     }
 
 }
-
 
 
 
